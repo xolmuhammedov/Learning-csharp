1f69e0ddcf26cfd709e6127063ea49d74e60d148
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-25 21:21:00 +0500

    writing first line of code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            // Visual Studio 2022 is sick tho
         }
     }
 }

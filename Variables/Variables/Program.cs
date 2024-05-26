f46eadbce16273d7ad5f394790f0bccbcbaa1d8c
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-26 13:50:27 +0500

    varaibles

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 100;
+            Console.WriteLine("Hello can i take " + firstNum + " $");
+            Console.ReadLine();
+
+        }
+    }
+}

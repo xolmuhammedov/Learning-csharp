c10709a516d543fdb04f35ac932d5e94ca7df7e6
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-05-26 13:46:24 +0500

    frist consoleapp

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // Visual Studio 2022 is sick tho
+            // Visual Studio 2022 is very sick tho
         }
     }
 }

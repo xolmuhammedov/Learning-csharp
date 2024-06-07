5ad71a740d9eb555be130c88e0e57c6a53b34b51
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-08 02:14:26 +0500

    learning default in switch

--- NIghtTimes/NIghtTimes/Program.cs
+++ NIghtTimes/NIghtTimes/Program.cs
@@ -10,15 +10,30 @@ namespace NIghtTimes
     {
         static void Main(string[] args)
         {
-            String fullName = "Bro Code";
-            String phonenumber = "123-456-7890";
-            phonenumber = phonenumber.Replace("-", "/");
-            Console.WriteLine(phonenumber);
+            //String fullName = "Bro Code";
+            //String phonenumber = "123-456-7890";
+            //phonenumber = phonenumber.Replace("-", "/");
+            //Console.WriteLine(phonenumber);
+            //int employeeId = 10;
+            int phonenumber = 10;
+            switch (phonenumber)
+            {
+                case 1:
+                    Console.WriteLine(false);
+                    break;
+                case 10:
+                    Console.Write(true);
+                    break;
+                    default:
+                    Console.WriteLine("Unkown");
+                    break;
+            }
 
             //fullName = fullName.ToUpper();
             //fullName = fullName.ToLower();
             //Console.WriteLine(fullName);
 
+            
             Console.ReadKey();
         }
     }

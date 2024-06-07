797dd5a66850a4671503dbaf2c5b68a310dd3c13
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-07 23:10:28 +0500

    being nervous

---
+++ NIghtTimes/NIghtTimes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIghtTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String fullName = "Bro Code";
+            String phonenumber = "123-456-7890";
+            phonenumber = phonenumber.Replace("-", "/");
+            Console.WriteLine(phonenumber);
+
+            //fullName = fullName.ToUpper();
+            //fullName = fullName.ToLower();
+            //Console.WriteLine(fullName);
+
+            Console.ReadKey();
+        }
+    }
+}

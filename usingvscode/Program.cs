e1f75671ad222e402c49282d07782fd2d223219f
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-09 16:11:50 +0500

    reviewing switch

--- usingvscode/Program.cs
+++ usingvscode/Program.cs
@@ -1,5 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello");
 
-int sum = 10;
-Console.WriteLine(sum);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Hello World!");
+}
+int userId = 3;
+switch (userId)
+{
+    case 3:
+        Console.WriteLine(true);
+    break;
+    default:
+        Console.WriteLine(false);
+     break;
+}

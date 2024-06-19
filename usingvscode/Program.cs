afe7daeeef37a68a83eae60e78563ab5f5c7f2ed
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-19 23:29:53 +0500

    starting the day

--- usingvscode/Program.cs
+++ usingvscode/Program.cs
@@ -2,9 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
-        bool userStatus = false;
-        Console.WriteLine(userStatus);
+        int person1 = 465;
+        Console.WriteLine(person1);
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadLine();
     }
 }
- 

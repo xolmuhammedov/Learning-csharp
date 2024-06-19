b6c2721e5a6035dc7c089f376f8d9a2db634b742
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-20 00:08:34 +0500

    reviewing the while

--- usingvscode/Program.cs
+++ usingvscode/Program.cs
@@ -2,10 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        int person1 = 465;
-        Console.WriteLine(person1);
-        for (int i = 0; i <= 10; i++)
+        int i = 0;
+        while (i < 10)
         {
+            i++;
             Console.WriteLine(i);
         }
 

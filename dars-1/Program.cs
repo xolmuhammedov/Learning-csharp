ce0eafc45f066d763742c4efebff1141cba487d3
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-06-29 23:30:11 +0500

    hmm arrays

--- dars-1/Program.cs
+++ dars-1/Program.cs
@@ -7,10 +7,13 @@
 //String - string
 
 int number = 10;
-byte number2 = 25;
 Console.WriteLine(number);
 
 string name = "julia";
 Console.WriteLine($"my name is  {name} " + 45);
+
+int[] numbers = {56,45,15};
+Console.WriteLine(numbers);
+
 
 Console.ReadLine();
